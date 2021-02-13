4ca389ef0eeef5d12bc23a9ce63fef92acd44a17
zinbrox <53508807+zinbrox@users.noreply.github.com>
2021-02-13 12:49:12 +0530

    Moved Theme Change Option from Settings to Side bar (sidenav)

--- lib/pages/side_nav.dart
+++ lib/pages/side_nav.dart
@@ -1,12 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app_2/blocs/color_styles.dart';
 import 'package:news_app_2/pages/home.dart';
+import 'package:provider/provider.dart';
 
-class SideDrawer extends StatelessWidget {
+class SideDrawer extends StatefulWidget {
+  @override
+  _SideDrawerState createState() => _SideDrawerState();
+}
+
+class _SideDrawerState extends State<SideDrawer> {
   @override
   Widget build(BuildContext context) {
     var firebaseUser =  FirebaseAuth.instance.currentUser;
+    DarkThemeProvider _themeChanger = Provider.of<DarkThemeProvider>(context);
+    bool isSwitched = _themeChanger.darkTheme;
+    String dropdownValue;
+    isSwitched ? dropdownValue = 'Dark' : dropdownValue = 'Light';
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -31,12 +42,23 @@ class SideDrawer extends StatelessWidget {
               ],
             ),
           ),
-          ListTile(
-            title: Text('Bookmarks'),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, '/bookmark');
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Theme"),
+              DropdownButton<String>(
+                value: dropdownValue,
+                onChanged: (String newValue){
+                  setState((){
+                    dropdownValue = newValue;
+                    newValue=='Dark' ? _themeChanger.darkTheme=true : _themeChanger.darkTheme=false;
+                  });
+                },
+                items: <String>['Light','Dark'].map<DropdownMenuItem<String>>((String value){
+                  return DropdownMenuItem<String>(value: value, child: Text(value),);
+                }).toList(),
+              ),
+            ],
           ),
           ListTile(
             title: Text('Settings'),
